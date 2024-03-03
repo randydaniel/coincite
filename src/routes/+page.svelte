ef18a33d21fb06ab3c0b5120e2baf035e7c3817d
@randydaniel <romeomikedelta@gmail.com>
2024-03-03 07:40:42 -0700

    Coming soon.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
+<script lang="ts">
+  import Terminal from "lucide-svelte/icons/terminal";
+  import * as Alert from "$lib/components/ui/alert";
+</script>
+
 <section>
   <div class="container h-screen">
     <div class="flex h-full items-center justify-center">
-      <h1>Coincite (Soon).</h1>
+      <div class="block">
+        <Alert.Root>
+          <Terminal class="h-4 w-4" />
+          <Alert.Title>Coincite — Coming soon!</Alert.Title>
+          <Alert.Description
+            >Expect a universe of innovation, a galaxy of creativity, and maybe, just maybe, a little bit of space magic.</Alert.Description
+          >
+        </Alert.Root>
+      </div>
     </div>
   </div>
 </section>
