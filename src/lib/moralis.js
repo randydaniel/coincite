a89ad4188f2ba86fe1107c17cfc7758e0088131f
@randydaniel <romeomikedelta@gmail.com>
2024-03-05 17:35:22 -0500

    Cleanup.

--- src/lib/moralis.js
+++
@@ -1,9 +0,0 @@
-import Moralis from 'moralis';
-
-// Replace these with your Moralis Application ID and Server URL
-const appId = import.meta.env.VITE_MORALIS_APPLICATION_ID;
-const serverUrl = import.meta.env.VITE_MORALIS_SERVER_URL;
-
-Moralis.start({ serverUrl, appId });
-
-export default Moralis;
